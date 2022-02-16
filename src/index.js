7d7b90cc4c2389bfe144de189aa47c83b6123b1a
temak22 <kireev.a.o@edu.mirea.ru>
2022-02-16 15:03:35 +0300

    7 task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 import RNCamera, { type Status as _CameraStatus, hasTorch } from './RNCamera';
 import FaceDetector from './FaceDetector';
 
-export type CamSt = _CameraStatus;
+export type CameraStatus = _CameraStatus;
 export {RNCamera, FaceDetector, hasTorch} ;
